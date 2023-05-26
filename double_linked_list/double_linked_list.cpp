37096df73280ff0eaaa1f27e14f4f8c66b36cc01
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-05-26 20:37:59 +0700

    menghapus data pada list

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -12,9 +12,9 @@ public:
 
 class DoubleLinkedList {
 private:
-    Node* START = NULL; 
+    Node* START; 
 public:
-
+    DoubleLinkedList();
     void addNode(); 
     bool search(int rollNo, Node** previous, Node** current);
     bool deleteNode(int rollNo);
@@ -25,15 +25,17 @@ public:
     void searchData();
 };
 
+DoubleLinkedList::DoubleLinkedList() {
+    START = NULL;
+}
+
 void DoubleLinkedList::addNode() {
     int nim;
     string nm;
-
     cout << "\nEnter the roll number of the student: ";
     cin >> nim;
     cout << "\nEnter the name of the student: ";
     cin >> nm;
-
     Node* newNode = new Node(); // step 1
     newNode->noMhs = nim; // step 2  
     newNode->name = nm; // step 2 
@@ -104,5 +106,110 @@ bool DoubleLinkedList::listEmpty() {
 
 void DoubleLinkedList::traverse() {
     if (listEmpty())
-        cout << "\n"
+        cout << "\nList is empty" << endl;
+    else {
+        cout << "\nRecords in ascending order of roll number are:" << endl;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
+}
+
+void DoubleLinkedList::revtraverse() {
+    if (listEmpty()) {
+        cout << "\nList is empty" << endl;
+    }
+
+    else {
+        cout << "\nRecords in descending order of roll number are:" << endl;
+        Node* currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+    }
+}
+
+void DoubleLinkedList::hapus() {
+    if (listEmpty()) {
+        cout << "\nList is empty" << endl;
+    }
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    int rollNo;
+    cin >> rollNo;
+    cout << endl;
+    if (DoubleLinkedList::deleteNode(rollNo) == false)
+        cout << "Record not found"  << endl;
+    else 
+        cout << "Record with roll number " << rollNo << "deleted" << endl;
+
+}
+
+void DoubleLinkedList::searchData() {
+    if (listEmpty() == true) {
+        cout << "\nList is empty" << endl;
+    }
+    Node* prev, * curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of the student whose record you want to search: ";
+    int num;
+    cin >> num;
+    if (DoubleLinkedList::search(num, &prev, &curr) == false)
+        cout << "\nRecord not found" << endl;
+    else {
+        cout << "\nRecord found" << endl;
+        cout << "\nRoll number: " << curr->noMhs << endl;
+        cout << "\nName: " << curr->name << endl;
+    }
+}
+
+int main() {
+    DoubleLinkedList obj;
+    while (true) {
+        try {
+            cout << "\nMenu" << endl;
+            cout << "1. Add a record to the list" << endl;
+            cout << "2. Delete a record from the list" << endl;
+            cout << "3. View all records in the ascending order of roll number" << endl;
+            cout << "4. View all records in the descending order of roll number" << endl;
+            cout << "5. Search for a record in the list" << endl;
+            cout << "6. Exit" << endl;
+            cout << "\nEnter your choice (1-6): ";
+            char ch;
+            cin >> ch;
+
+            switch (ch) {
+            case '1':
+                obj.addNode();
+                break;
+            case '2':
+                obj.hapus();
+                break;
+            case '3':
+                obj.traverse();
+                break;
+            case '4':
+                obj.revtraverse();
+                break;
+            case '5':
+                obj.searchData();
+                break;
+            case '6':
+                return 0;
+            default:
+                cout << "\nInvalid option" << endl;
+                break;
+            }
+
+        }
+        catch (exception& e) {
+            cout << "Check for the values entered." << endl;
+
+        }
+    }
 }
