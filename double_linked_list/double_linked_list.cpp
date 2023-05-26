05568ea7782f50ae9c2a9c62d80babd444f38071
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-05-26 15:50:38 +0700

    mengalokasi  memory untuk membuat object baru

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -12,12 +12,30 @@ public:
 
 class DoubleLinkedList {
 private:
-    node* START = NULL; 
+    Node* START = NULL; 
 public:
 
     void addNote(); 
+    bool search(int rollNo, Node** previous, Node** current);
+    bool deleteNode(int rollNo);
+    bool listEmpty();
+    void traverse();
+    void revtraverse();
+    void hapus();
+    void searchData();
 };
 
+void DoubleLinkedList::addNode() {
+    int nim;
+    string nm;
+    cout << "\nEnter the roll number of the student: ";
+    cin >> nim;
+    cout << "\nEnter the name of the student: ";
+    cin >> nm;
+
+    Node* newNode = new Node(); // step 1
+    newNode->noMhs = nim; // step 2
+}
 int main()
 {
     std::cout << "Hello World!\n";
